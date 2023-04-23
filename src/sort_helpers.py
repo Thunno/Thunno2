a09cd62e11afbc5c0b961fa6e95936c1d2c4a46f
nayakrujul <nayakrujul@hotmail.com>
2023-04-23 20:18:09 +0100

    Sort helpers.py

---
+++ src/sort_helpers.py
@@ -0,0 +1,38 @@
+# Inspired by Vyxal (https://github.com/Vyxal/Vyxal/blob/main/sort_element_functions.py)
+
+import re
+
+f = open("thunno2/helpers.py", "r", encoding="utf-8")
+file = f.read()
+f.close()
+
+# Just in case this fails
+f = open("thunno2/helpers_backup.py", "w+", encoding="utf-8")
+f.write(file)
+f.close()
+
+fns = [
+    x.strip()
+    for x in re.findall(
+        "(?:@.+\n)*def +[a-zA-Z_][a-zA-Z_\d]*\s*\([\S\s]*?\)(?: *-> *.+?)?\:(?:\n(?:(?:    |\t).*)?)+",
+        file,
+    )[2:]
+]
+
+file = file.split("\n")
+
+start_line = file.index(fns[0].split("\n")[0])
+last_line = file.index(fns[-1].split("\n")[-1])
+
+f = open("thunno2/helpers.py", "w", encoding="utf-8")
+
+f.write(
+    "\n".join(file[:start_line])
+    + "\n"
+    + "\n\n\n".join(sorted(fns, key=lambda x: x[x.index("def ") :])).strip()
+    + "\n\n\n"
+    + "\n".join(file[last_line + 1 :]).strip()
+    + "\n"
+)
+
+f.close()
