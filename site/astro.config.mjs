c9decf57ce163fa4bc9ae311921c0cb7ca02f796
Jacob <jacobulysseslockwood@gmail.com>
2023-05-15 12:19:20 -0400

    support mathjax in markdown

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,12 +1,20 @@
+// @ts-check
+
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import rehypeMathJaxSvg from "rehype-mathjax";
+import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://thunno.github.io",
   base: "/thunno2",
   integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathJaxSvg],
+  },
   vite: {
     resolve: {
       preserveSymlinks: true,
